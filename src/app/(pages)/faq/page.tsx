3d8de81a8fc9a9221168dcbcbd98eedddbdf3e51
derey79 <devyreymond@gmail.com>
2026-06-27 11:14:24 +0700

    add new page for faq

---
+++ src/app/(pages)/faq/page.tsx
@@ -0,0 +1,15 @@
+import Testimonials from '@/app/components/Testimonials';
+import FAQSection from '@/app/components/FAQSection';
+import React from 'react';
+
+function TestimonialPage() {
+  return (
+    <div className='py-12'>
+      <Testimonials />
+
+      <FAQSection />
+    </div>
+  );
+}
+
+export default TestimonialPage;
