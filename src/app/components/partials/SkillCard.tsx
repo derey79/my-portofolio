fe96cb7a2f061fe3092b5b61c3924f64c7e8f6fc
derey79 <devyreymond@gmail.com>
2026-06-27 09:11:42 +0700

    modified Skill card component

--- src/app/components/partials/SkillCard.tsx
+++ src/app/components/partials/SkillCard.tsx
@@ -1,6 +1,11 @@
+'use client';
+import React from 'react';
+// import { useState } from 'react';
 import Image from 'next/image';
 // import { SkillItem } from '@/app/constants/skills';
 // import { Skill } from '@/types/types';
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
 
 interface SkillCardProps {
   name: string;
@@ -13,8 +18,18 @@ export default function SkillCard({
   iconPath,
   percentage,
 }: SkillCardProps) {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  const [isCounting, setIsCounting] = React.useState(false);
+
   return (
-    <div className='flex items-center justify-between w-full py-5 border-b border-neutral-800/60 text-white transition-all duration-200 hover:bg-white/1 px-2 group'>
+    <div
+      ref={ref}
+      className='flex items-center justify-between w-full py-5 border-b border-neutral-800/60 text-white transition-all duration-200 hover:bg-white/1 px-2 group'
+    >
       <div className='flex items-center gap-4'>
         <div className='relative flex items-center justify-center overflow-hidden rounded-md w-15 h-15 bg-neutral-900/30'>
           <Image
@@ -31,8 +46,26 @@ export default function SkillCard({
         </span>
       </div>
 
-      <span className='font-mono text-base font-bold tracking-wider text-neutral-100 transition-colors group-hover:text-[#1ba4ca]'>
-        {percentage}%
+      <span
+        className={`font-mono text-base font-bold tracking-wider transition-colors duration-300 ${
+          isCounting
+            ? 'text-red-500' // Warna merah saat angka sedang berjalan
+            : 'text-neutral-100 group-hover:text-[#1ba4ca]' // Warna putih standar & efek hover setelah selesai
+        }`}
+      >
+        {inView ? (
+          <CountUp
+            start={0}
+            end={percentage}
+            duration={3}
+            suffix='%'
+            onStart={() => setIsCounting(true)}
+            onEnd={() => setIsCounting(false)}
+          />
+        ) : (
+          '0%'
+        )}
+        {/* <CountUp end={percentage} duration={2} suffix='%' /> */}
       </span>
     </div>
   );
