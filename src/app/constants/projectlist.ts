48aa031ec107c0105d98d9d79ecd3015cda36910
derey79 <devyreymond@gmail.com>
2026-06-27 11:20:52 +0700

    add new const for project list

---
+++ src/app/constants/projectlist.ts
@@ -0,0 +1,20 @@
+export const PROJECTS = [
+  {
+    title: 'Portfolio 1',
+    year: '2025',
+    image: '/portfolio-1.png',
+    slug: 'portfolio-1',
+  },
+  {
+    title: 'Portfolio 2',
+    year: '2025',
+    image: '/portfolio-2.png',
+    slug: 'portfolio-2',
+  },
+  {
+    title: 'Portfolio 3',
+    year: '2025',
+    image: '/portfolio-3.png',
+    slug: 'portfolio-3',
+  },
+];
