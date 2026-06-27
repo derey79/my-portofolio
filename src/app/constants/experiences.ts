03e6db7e2dd14134a9b8d068eb9d87cbe14ce61c
derey79 <devyreymond@gmail.com>
2026-06-27 11:19:54 +0700

    add new type interface for data experiences

---
+++ src/app/constants/experiences.ts
@@ -0,0 +1,23 @@
+export const experiences = [
+  {
+    title: 'Frontend Developer',
+    period: '2025 - Present',
+    description:
+      'Built responsive web interfaces using modern frameworks like React.js, ensuring seamless integration with backend systems. Optimized performance, implemented accessible designs, and delivered clean, reusable code to enhance user experience and scalability.',
+    companyLogo: '/airbnb.png',
+  },
+  {
+    title: 'Frontend Developer',
+    period: '2024 - 2025',
+    description:
+      'Built responsive web interfaces using modern frameworks like React.js, ensuring seamless integration with backend systems. Optimized performance, implemented accessible designs, and delivered clean, reusable code to enhance user experience and scalability.',
+    companyLogo: '/dribble.png',
+  },
+  {
+    title: 'Frontend Developer',
+    period: '2023 - 2024',
+    description:
+      'Built responsive web interfaces using modern frameworks like React.js, ensuring seamless integration with backend systems. Optimized performance, implemented accessible designs, and delivered clean, reusable code to enhance user experience and scalability.',
+    companyLogo: '/zoom.png',
+  },
+];
