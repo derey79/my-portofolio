36f091e54578477ae86c101b97c5b91efaba72df
derey79 <devyreymond@gmail.com>
2026-06-27 11:17:10 +0700

    create new file for TitleHeader

---
+++ src/app/components/commons/TitleHeader.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react';
+
+interface SectionHeaderProps {
+  title: ReactNode;
+  description: ReactNode;
+  align?: 'left' | 'center';
+}
+
+export default function TitleHeader({
+  title,
+  description,
+  align = 'center',
+}: SectionHeaderProps) {
+  return (
+    <div
+      className={`mb-12 ${align === 'center' ? 'text-center' : 'text-left'}`}
+    >
+      <h2 className='section-title pb-4'>{title}</h2>
+
+      <p className='section-description'>{description}</p>
+    </div>
+  );
+}
